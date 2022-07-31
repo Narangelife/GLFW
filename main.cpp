3a88c66a6a890ae664d99dee3d8dff8f9dc091ca
Narangelife <narange@scriptarts.jp>
2022-07-31 18:26:43 +0900

    CMake setup for GLEW

--- main.cpp
+++ main.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
-#include <glew.h>
-#include <glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    // 初期化 失敗?
     if (glfwInit() == GL_FALSE){
+        std::cerr << "Initialize GLFW error" << std::endl;
         return 1;
     }
 
-    GLFWwindow *const window(glfwCreateWindow(640, 480, "Taku", NULL, NULL));
-    if (window == NULL){
+    atexit(glfwTerminate); // プログラム終了時の処理登録
+
+    // ウィンドウの作成
+    GLFWwindow *const window(glfwCreateWindow(640, 480, "TEST", nullptr, nullptr));
+    if (window == nullptr){
+        std::cerr << "Window GLFW error" << std::endl;
         return 1;
     }
-    glfwMakeContextCurrent(window);
+
+    glfwMakeContextCurrent(window); //ウィンドウのOpenGL処理対称指定
+
+    // GLEW 有効化・初期化
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK){
+        std::cerr << "Initialize GLEW error" << std::endl;
+        return 1;
+    }
+
     glClearColor(0.1f, 0.1f, 0.1f, 0.f);
 
+    // ウィンドウフレーム描画処理
     while (glfwWindowShouldClose(window) == GL_FALSE){
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT); //ウィンドウのクリア
 
-        glfwSwapBuffers(window);
-        glfwWaitEvents();
+        /* ------------- 描画処理 -------------*/
+
+        glfwSwapBuffers(window); // カラーバッファの入れ替え
+        glfwWaitEvents(); //イベントの取り出し
     }
     return 0;
 }
